0137fbc94736143e8b4a7a83ea71e5b1d6f9e74a
maxluna12 <78857305+maxluna12@users.noreply.github.com>
2022-06-12 14:17:40 -0300

    Add files via upload

--- Suma.py
+++ Suma.py
@@ -1,4 +1,8 @@
-# Suma 2 paràmetros.
-A = input('Ingrese un nùmero ')
-B = input('Ingrese un segundo nùmero ')
-print(A + B)
+# Suma 2 paràmetro
+import math
+# parametro1 + parametro
+def Suma():
+    parametro1 = int(input('Ingrese un numero  '))
+    parametro2 = int(input('Ingrese otro nùmero  '))
+    suma = parametro1 + parametro2
+    print('El resulado de la suma es', suma)
